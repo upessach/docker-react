d957e6b999c2949128e6d799d3af46c16cbc4bfe
split <a@a.com>
2022-11-16 15:06:00 +0200

    changed App.js to blah blah blah

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
+        <p>Blah Blah Blah</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          blah blah
+          Learn React
         </a>
       </header>
     </div>
